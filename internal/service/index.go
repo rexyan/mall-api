9bb4c3500224175e473de3a8fb7ad5035f6d5935
runsha.yan <runsha.yan@cyclone-robotics.com>
2022-07-07 11:39:19 +0800

    2022.07.07 v1.0.0, 更新首页商品查询 SQL

--- internal/service/index.go
+++ internal/service/index.go
@@ -22,41 +22,33 @@ func Index() *sIndex {
 
 /**
 获取首页轮播图信息
- */
+*/
 func (s *sIndex) GetCarousels(ctx context.Context) ([]v1.IndexCarouselItem, error) {
 	var carousels []v1.IndexCarouselItem
 	err := dao.Carousel.Ctx(ctx).Where(do.Carousel{
-		IsDeleted:  0,
+		IsDeleted: 0,
 	}).Scan(&carousels)
 	return carousels, err
 }
 
 /**
 获取首页商品信息
- */
+*/
 func (s *sIndex) GetIndexGoods(ctx context.Context, indexGoodsType int) ([]v1.GoodsItem, error) {
 	var goods []v1.GoodsItem
 
-	// 查询首页热门商品ID
-	goodsIds, err := g.Model("tb_newbee_mall_index_config").Fields("goods_id").Where(g.Map{"is_deleted": 0, "config_type": 3}).Array()
+	err := g.Model("tb_newbee_mall_index_config index_config").InnerJoin("tb_newbee_mall_goods_info goods", "index_config.goods_id=goods.goods_id").Fields(
+		"goods.*",
+		).Where(g.Map{"index_config.config_type": indexGoodsType}).Scan(&goods)
 	if err != nil {
 		return nil, err
 	}
-	for _, id := range goodsIds{
-		var goodsItem v1.GoodsItem
-		err := g.Model("tb_newbee_mall_goods_info").Where(g.Map{"goods_id": id}).Scan(&goodsItem)
-		if err != nil {
-			return nil, err
-		}
-		goods = append(goods, goodsItem)
-	}
-
 	return goods, err
 }
 
 /**
 获取首页热门商品信息
- */
+*/
 func (s *sIndex) GetHotGoods(ctx context.Context) ([]v1.GoodsItem, error) {
 	return s.GetIndexGoods(ctx, 3)
 }
